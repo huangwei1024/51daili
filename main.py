2aecfabb53f9a264041de28411ab8cab89f8314d
HuangWei <sir.huangwei@gmail.com>
2015-09-11 19:57:09 +0800

    create repo

---
+++ main.py
@@ -0,0 +1,52 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+import re
+import tornado.auth
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado.options import define, options
+from tornado.log import app_log
+
+import time
+import itertools
+import datetime
+from collections import defaultdict
+
+define("port", default=808, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", IndexHandler),
+        ]
+        settings = dict(
+            blog_title=u"51daili.info",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            cookie_secret="51_daili",
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    pass
+
+class IndexHandler(BaseHandler):
+    def get(self):
+        self.render('index.html')
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
